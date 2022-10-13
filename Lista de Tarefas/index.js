70b63b0533d0f7c1d0bcf26d0c534fdc07dbaad4
Astatsu <vinijao123690@gmail.com>
2022-10-13 16:56:39 -0300

    opaopa

---
+++ Lista de Tarefas/index.js
@@ -0,0 +1,9 @@
+let adicionar = document.getElementById("adicionar");
+
+let listaDeTarefas = [];
+
+adicionar.addEventListener("click", adicionarTarefa)
+
+function adicionarTarefa() {
+    let tarefa = prompt("Digite a tarefa")
+}
