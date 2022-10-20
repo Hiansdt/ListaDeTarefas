d59f38ea32d83268135ebf39bb6b30103a313545
Hiansdt <106831397+Astatsu@users.noreply.github.com>
2022-10-20 16:57:51 -0300

    Add files via upload

--- Lista de Tarefas/index.js
+++ Lista de Tarefas/index.js
@@ -3,14 +3,13 @@ const tarefas = document.getElementById("tarefas");
 let tituloValor = document.getElementById('titulo');
 let descValor = document.getElementById('desc');
 let dataValor = document.getElementById('data');
+let prioridadeValor = document.getElementById('prioridade')
 const adicionarBotao = document.getElementById("adicionador");
+let prioridadeDiv1 = document.querySelector(".prioridade1");
+let prioridadeDiv2 = document.querySelector(".prioridade2");
+let prioridadeDiv3 = document.querySelector(".prioridade3");
 
-titulo.className = "input";
-desc.className = "input";
-data.className = "input";
-
-
-adicionarBotao.addEventListener("click", addTarefa);2
+adicionarBotao.addEventListener("click", addTarefa);
 
 listaCriador.style.display = "flex";
 
@@ -21,38 +20,62 @@ function addTarefa() {
     let tarefa = document.createElement("div");
     tarefa.className = "azul";
     tarefa.id = i;
-    tarefas.appendChild(tarefa);
+
     let titulo = document.createElement("div");
     let desc = document.createElement("div");
     let data = document.createElement("div");
+    let prioridade = document.createElement("div");
+
     titulo.className = "input";
     desc.className = "input descricao";
     data.className = "input";
+    prioridade.style.border = "1px solid white"
+
     titulo.innerHTML = tituloValor.value;
     desc.innerHTML = descValor.value;
     data.innerHTML = dataValor.value;
+    prioridade.innerHTML = "prioridade: " + prioridadeValor.value
+
     tarefa.appendChild(titulo);
     tarefa.appendChild(desc);
     tarefa.appendChild(data);
+    tarefa.appendChild(prioridade);
+
     tituloValor.value = "";
     descValor.value = "";
     dataValor.value = "";
-    tituloText = "Título";
-    descText = "Descrição";
-    dataText = "Prazo";
-    titulo.insertAdjacentText("beforebegin", tituloText);
-    desc.insertAdjacentText("beforebegin", descText);
-    data.insertAdjacentText("beforebegin", dataText);
+
+    titulo.insertAdjacentText("beforebegin", "Título");
+    desc.insertAdjacentText("beforebegin", "Descrição");
+    data.insertAdjacentText("beforebegin", "Prazo");
+
     let deletar = document.createElement("button");
     deletar.id = "d" + i;
     deletar.innerHTML = "Deletar tarefa";
+
     let tarefaCompleta = document.createElement("input");
+
     tarefaCompleta.setAttribute("type", "checkbox");
+    tarefaCompleta.className = "tarefaComp";
     tarefa.appendChild(tarefaCompleta);
     tarefa.appendChild(deletar);
+
     deletar.addEventListener("click", () => {
       deletarTarefa(deletar.id);
     } );
+    
+    if(prioridadeValor.value == 1) {
+      tarefas.appendChild(tarefa);
+      tarefa.classList.add("pr1");
+    }
+
+    let prel = document.querySelector(".pr1") 
+
+    if (prioridadeValor.value == 2) {
+      tarefas.insertBefore(tarefa, prel);
+    } else if (prioridadeValor.value == 3) {
+      tarefas.insertBefore(tarefa, tarefas.firstChild);
+    } 
 }
 
 function deletarTarefa(DivDeletar) {
